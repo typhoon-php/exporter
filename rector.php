7b3c98d3f74d8baa28adf2e8417ebdd480fcda40
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2023-11-21 11:54:09 +0300

    Do not rector tests/values

--- rector.php
+++ rector.php
@@ -13,6 +13,9 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ]);
+    $rectorConfig->skip([
+        __DIR__ . '/tests/values',
+    ]);
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_81,
         PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
